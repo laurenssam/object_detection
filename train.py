eda5da3b7f71d490995b1c0bf934a01a486c874f
Laurens Samson <laurenssamson@hotmail.com>
2020-04-10 14:56:05 +0200

    batch_size to 32

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Learning parameters
 checkpoint = None  # path to model checkpoint, None if none
-batch_size = 2  # batch size
+batch_size = 32  # batch size
 iterations = 120000  # number of iterations to train
 workers = 4  # number of workers for loading data in the DataLoader
 print_freq = 3  # print training status every __ batches
