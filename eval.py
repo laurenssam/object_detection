c1ecdab5d3104016d101feabab6d9cab0d067e93
Laurens Samson <laurenssamson@hotmail.com>
2020-04-12 09:30:13 +0200

    save on cpu

--- eval.py
+++ eval.py
@@ -8,20 +8,10 @@ from pprint import PrettyPrinter
 pp = PrettyPrinter()
 
 # Parameters
-data_folder = "/content/data/VOCdevkit"
 keep_difficult = True  # difficult ground truth objects must always be considered in mAP calculation, because these objects DO exist!
 batch_size = 32
 workers = 2
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-checkpoint = '/content/gdrive/My Drive/checkpoint_ssd300.pth.tar'
-
-# Load model checkpoint that is to be evaluated
-checkpoint = torch.load(checkpoint)
-model = checkpoint['model']
-model = model.to(device)
-
-# Switch to eval mode
-model.eval()
 
 def evaluate(test_loader, model):
     """
@@ -57,13 +47,13 @@ def evaluate(test_loader, model):
             # Evaluation MUST be at min_score=0.01, max_overlap=0.45, top_k=200 for fair comparision with the paper's results and other repos
 
             # Store this batch's results for mAP calculation
-            boxes = [b.to(device) for b in boxes]
-            labels = [l.to(device) for l in labels]
-            difficulties = [d.to(device) for d in difficulties]
+            boxes = [b.cpu() for b in boxes]
+            labels = [l.cpu()for l in labels]
+            difficulties = [d.cpu() for d in difficulties]
 
-            det_boxes.extend(det_boxes_batch)
-            det_labels.extend(det_labels_batch)
-            det_scores.extend(det_scores_batch)
+            det_boxes.extend([box.cpu() for box in det_boxes_batch])
+            det_labels.extend([label.cpu() for label in det_labels_batch])
+            det_scores.extend([score.cpu() for score in det_scores_batch])
             true_boxes.extend(boxes)
             true_labels.extend(labels)
             true_difficulties.extend(difficulties)
@@ -78,7 +68,7 @@ def evaluate(test_loader, model):
     model.train()
 
 if __name__ == '__main__':
-    run_local = False
+    run_local = True
     if run_local:
         voc_2007 = Path("/Users/laurenssamson/Documents/Projects/Chess_notation/pascal_VOC/VOCdevkit/VOC2007")
         voc_test = Path("/Users/laurenssamson/Documents/Projects/Chess_notation/pascal_VOC/VOCdevkit_test/VOC2007")
@@ -86,6 +76,7 @@ if __name__ == '__main__':
         voc_2012 = Path("/Users/laurenssamson/Documents/Projects/Chess_notation/pascal_VOC/VOCdevkit2012/VOC2012")
         out_path = Path("/Users/laurenssamson/Documents/Projects/Chess_notation/object_detecion/json")
         data_folder = './json'  # folder with data files
+        checkpoint = 'checkpoint_ssd300.pth.tar'
 
     else:
         voc_2007 = Path("/content/data/VOCdevkit/VOC2007")
@@ -94,6 +85,7 @@ if __name__ == '__main__':
         voc_2012 = Path("/content/data/VOCdevkit/VOC2012")
         out_path = Path("/content/data/VOCdevkit")
         data_folder = "/content/data/VOCdevkit"  # folder with data files
+        checkpoint = '/content/gdrive/My Drive/checkpoint_ssd300.pth.tar'
 
     out_path.mkdir(exist_ok=True)
     create_data_lists(voc_2007, voc_2012, voc_test, out_path)
@@ -102,5 +94,12 @@ if __name__ == '__main__':
                                     keep_difficult=keep_difficult)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
                                               collate_fn=test_dataset.collate_fn, num_workers=workers, pin_memory=True)
+    # Load model checkpoint that is to be evaluated
+    checkpoint = torch.load(checkpoint)
+    model = checkpoint['model']
+    model = model.to(device)
+
+    # Switch to eval mode
+    model.eval()
     evaluate(test_loader, model)
 
